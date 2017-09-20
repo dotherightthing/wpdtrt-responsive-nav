1d1e269f88c31dbd934c05ce3df9e53567ff6c57
Dan Smith <dev@dotherightthing.co.nz>
2017-09-20 12:06:57 +1200

    Use SCSS; Add menu icon for toggle button;

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,81 @@
 /**
- * gulpfile.js
- * Run tasks to aid the development process.
+ * Gulp Task Runner
+ * Compile front-end resources
+ *
  * @example gulp default
+ * @example gulp css
+ * @example gulp php
+ *
+ * This file contains JavaScript.
+ *
+ * @link        https://github.com/dotherightthing/wpdtrt-responsive-nav
+ * @see         https://codex.wordpress.org/AJAX_in_Plugins
+ * @since       0.1.0
+ *
+ * @package     Wpdtrt_Responsive_Nav
  */
-var gulp = require('gulp');
-var phplint = require('gulp-phplint');
 
-gulp.task('default', function () {
-  return gulp.src(['**/*.php'])
+// dependencies
+var gulp = require('gulp');
+var autoprefixer = require('autoprefixer');
+var phplint = require('gulp-phplint');
+var postcss = require('gulp-postcss');
+var pxtorem = require('postcss-pxtorem');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+
+// parent theme source directories
+var scssSrc = './scss/*.scss';
+
+// target directories
+var cssDir = './css/';
+var phpDir = '**/*.php';
+
+// tasks
+
+gulp.task('css', function () {
+
+  var processors = [
+      autoprefixer({
+        cascade: false
+      }),
+      pxtorem({
+        rootValue: 16,
+        unitPrecision: 5,
+        propList: [
+          'font',
+          'font-size',
+          'padding',
+          'padding-top',
+          'padding-right',
+          'padding-bottom',
+          'padding-left',
+          'margin',
+          'margin-top',
+          'margin-right',
+          'margin-bottom',
+          'margin-left',
+          'bottom',
+          'top',
+          'max-width'
+        ],
+        selectorBlackList: [],
+        replace: false,
+        mediaQuery: true,
+        minPixelValue: 0
+      })
+  ];
+
+  return gulp
+    .src(scssSrc)
+    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest(cssDir));
+});
+
+gulp.task('php', function () {
+  return gulp
+    .src([phpDir])
 
     // validate PHP
     // The linter ships with PHP
@@ -20,3 +88,6 @@ gulp.task('default', function () {
       }
     }));
 });
+
+gulp.task( 'default', ['css'] );
+
